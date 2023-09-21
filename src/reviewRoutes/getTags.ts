15861b68e338380fed09b1c4f875b0904c5c2e0c
artyZinchenko <zinchenko.artem96@gmail.com>
2023-09-21 20:47:48 +0400

    returns most popular tags

--- src/reviewRoutes/getTags.ts
+++ src/reviewRoutes/getTags.ts
@@ -8,7 +8,12 @@ const route = express.Router();
 export default route.get('/tags', async (req, res, next) => {
     const prisma = req.prisma;
     try {
-        const allTags = await prisma.tag.findMany();
+        const allTags = await prisma.tag.findMany({
+            orderBy: {
+                usage: 'desc',
+            },
+            take: 12,
+        });
         res.status(200).json({ tags: allTags });
     } catch (error) {
         console.error('Error fetching records:', error);
